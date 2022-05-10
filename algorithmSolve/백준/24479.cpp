91dc927be2083032bd55801b94b3ba434b3c1e27
홍지학 <5226947@hanmail.net>
2022-05-10 14:42:18 +0900

    알고리즘 문제 풀이

---
+++ algorithmSolve/백준/24479.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> answer;
+vector<vector<int>> board;
+vector<bool> check;
+int point = 1;
+
+void dfs(int here)
+{
+    check[here] = true;
+    answer[here] = point;
+    ++point;
+    for (int i = 0; i < board[here].size(); ++i)
+    {
+        int there = board[here][i];
+        if (!check[there])
+            dfs(there);
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n, m, r;
+    cin >> n >> m >> r;
+    board.assign(n, vector<int>());
+    check.assign(n, false);
+    answer.assign(n, 0);
+    for (int i = 0; i < m; ++i)
+    {
+        int a, b;
+        cin >> a >> b;
+        board[a - 1].push_back(b - 1);
+        board[b - 1].push_back(a - 1);
+    }
+    for (int i = 0; i < n; ++i)
+        sort(board[i].begin(), board[i].end());
+    dfs(r - 1);
+    for (int i = 0; i < n; ++i)
+        cout << answer[i] << '\n';
+    return 0;
+}
