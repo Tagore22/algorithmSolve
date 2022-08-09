8d8c1becf1b7a7ca660bcd849983b997b52f70d9
홍지학 <5226947@hanmail.net>
2022-08-09 14:12:32 +0900

    알고리즘 문제풀이(그리디, 우선순위 큐)

--- algorithmSolve/백준/1826.cpp
+++ algorithmSolve/백준/1826.cpp
@@ -6,6 +6,9 @@ using namespace std;
 int N, goal, fuel;
 priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> que;
 
+//현 연료로 도달할수 있는 모든 주유소를 파악후(우선순위 큐에 거리순으로 모아둠), 그 주유소중
+//가장 많은 연료를 얻을수 있는 곳에 도달하여(우선순위 큐에서 제외됨) 연료를 추가한다.
+
 void MakeAnswer() //pair<int,int> = <거리,연료>
 {
     int answer = 0;
