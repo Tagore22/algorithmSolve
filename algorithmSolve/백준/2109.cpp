6178ac278fab93dcebcee02846a0de5305708d33
홍지학 <5226947@hanmail.net>
2022-09-01 15:41:29 +0900

    알고리즘 문제 풀이(그리디, 우선순위 큐)

--- algorithmSolve/백준/2109.cpp
+++ algorithmSolve/백준/2109.cpp
@@ -7,6 +7,10 @@ using namespace std;
 int N;
 pair<int, int> board[10000];
 
+//1781번과 매우 비슷한 문제이다. 풀이까지 똑같은데 조금 다른점이라면 이문제에선 day라는 변수를 통해서 현재의 날짜를
+//추적해 나가며 풀었으나, 1781번에서는 큐의 크기로 대체했다는 점이다. 어차피 day는 직접적으로 큐에 삽입되는 첫번째
+//if문에서만 증가해야 함으로 둘다 다를바가 없다는 점이다.
+
 void MakeAnswer() //d, p 순
 {
     priority_queue<int> que;
