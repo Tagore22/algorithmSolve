f928638c51e7cb50e4ab2c29a843411b6132c94d
홍지학 <5226947@hanmail.net>
2022-09-07 13:48:01 +0900

    알고리즘 문제 풀이(정수론, 피사노 주기2)

--- algorithmSolve/백준/2749.cpp
+++ algorithmSolve/백준/2749.cpp
@@ -1,39 +1,27 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-vector<int> cashe;
-int P, M;
+//9471번 피사노 주기와 연계되는 문제이다. 피사노 주기를 통해서 피보나치 수열을 백만으로 나누면 주기는 150만임을 알수 있으므로
+//피보나치 수열값을 100만으로 나눈 150만개의 배열을 미리 계산해놓고, 주어진 n을 150만으로 나누어 떨어지는 값을 인덱스로 하여
+//배열값을 출력하면 된다.
 
-//i-2 + i-1 % M으로 피보나치 수열을 만들어가며 0과 1이 발견될때 i가 구하고자 하는 주기이다.
-//나눗셈을 사용하기에 0과 1이 발견되는 순간이 주기가 맞음을 증명할수 있다.
+int cashe[1500010];
+long long N;
+const int MODNUM = 1000000;
 
 void CalculFibo()
 {
-    cashe.clear();
-    cashe.push_back(0);
-    cashe.push_back(1);
-    int idx = 3;
-    while (true)
-    {
-        cashe.push_back((cashe[idx - 3] + cashe[idx - 2]) % M);
-        if (cashe[idx - 2] == 0 && cashe[idx - 1] == 1)
-            break;
-        ++idx;
-    }
-    cout << idx - 2 << '\n';
+    cashe[0] = 0;
+    cashe[1] = 1;
+    for (int i = 2; i < 1500005; ++i)
+        cashe[i] = (cashe[i - 2] + cashe[i - 1]) % MODNUM;
 }
 
 void MakeAnswer()
 {
-    int idx;
-    for (int i = 0; i < P; ++i)
-    {
-        cin >> idx >> M;
-        cout << idx << " ";
-        CalculFibo();
-    }
+    CalculFibo();
+    cout << cashe[N % 1500000] << '\n';
 }
 
 void Input()
@@ -42,7 +30,7 @@ void Input()
     cin.tie(0);
     cout.tie(0);
 
-    cin >> P;
+    cin >> N;
 }
 
 int main()
