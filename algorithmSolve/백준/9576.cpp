4ad258d1fe05221a0d2479b573efaefcc1d26359
홍지학 <5226947@hanmail.net>
2023-02-18 15:01:37 +0900

    알고리즘 문제 풀이(그리디 알고리즘, 이분매칭)

--- algorithmSolve/백준/9576.cpp
+++ algorithmSolve/백준/9576.cpp
@@ -11,6 +11,11 @@ using namespace std;
 // 다시 말해, 범위 값이 아닌 신청서의 끝값을 기준으로 정렬해야했다. 또한 끝값이 같다면 시작값이 적은순으로 정렬한다.
 // 그후 신청서의 시작값부터 끝값까지 순회하여 줄수 있는 책의 수를 체크하여 매 테스트 케이스마다 출력하면 된다.
 
+// P.S 나 자신의 이해를 좀더 깊게 하기 위해 추가한다.
+// 기본적으로 범위가 좁은 신청서의 학생에게 우선적으로 책을 주어야 하는데, 범위는 신청서의 끝값 - 시작값이라고 생각할수 있으나
+// 실제로는 그렇지 않다. 앞에서부터(1번책부터) 책을 주기 때문에 끝값 자체가 범위가 된다. 그래서 시작값은 상관없이 끝값만을
+// 기준으로 하여 정렬하면 된다.
+
 vector<pair<int, int>> board;
 int T, N, M;
 
@@ -19,7 +24,7 @@ bool CompareFunc(pair<int, int> lhs, pair<int, int> rhs)
     if (lhs.second != rhs.second)
         return lhs.second < rhs.second;
     else
-        return lhs.first < rhs.first;
+        return lhs.first < rhs.first; // 시작값은 상관없기에 굳이 신경쓰지 않아도 된다.
 }
 
 void CalAnswer()
