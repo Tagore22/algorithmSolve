4339da3f8646fd937c6509d17a1b13690f671dc7
홍지학 <5226947@hanmail.net>
2024-03-11 14:01:21 +0900

    알고리즘 24263번 문제 풀이(구현, 시뮬레이션) 2024. 3. 11

---
+++ algorithmSolve/백준/24263.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+// 풀이.
+// for문이 1개밖에 없으므로 무조건 1차원 순회에 그 길이는 N이다.
+
+int main()
+{
+    int N;
+    cin >> N;
+    cout << N << '\n';
+    cout << 1 << '\n';
+    return 0;
+}
