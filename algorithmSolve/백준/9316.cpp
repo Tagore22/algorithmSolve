cd38f493ed911c8e23f4f1b2aee565ef159db27c
홍지학 <5226947@hanmail.net>
2024-03-13 10:16:45 +0900

    알고리즘 9316번 문제 풀이(구현) 2024. 3. 13

---
+++ algorithmSolve/백준/9316.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+// 풀이.
+// 주어진 문자열을 주어진 수만큼 출력하는 문제.
+
+int main()
+{
+    int N;
+    cin >> N;
+    for (int i = 1; i <= N; ++i)
+        cout << "Hello World, Judge " << i << '!' << '\n';
+    return 0;
+}
