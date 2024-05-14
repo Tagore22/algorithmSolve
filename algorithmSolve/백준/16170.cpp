f7f03ebdb0d79fa4f5821d8bbbc7bccefb075c47
홍지학 <5226947@hanmail.net>
2024-05-14 12:14:49 +0900

    알고리즘 16170번 문제 풀이(구현) 2024. 5. 14

---
+++ algorithmSolve/백준/16170.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+// 풀이.
+// 우리나라는 UTC+0보다 9시간 느리다. 따라서 현재 시간이 09시보다 이르다면 하루전을,
+// 그렇지 않다면 오늘 날짜를 출력하면 된다.
+
+int main()
+{
+    cout << 2024 << '\n';
+    cout << 05 << '\n';
+    cout << 14 << '\n';
+    return 0;
+}
