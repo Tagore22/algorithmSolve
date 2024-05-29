7f1814c32b6d5866721e7cfcf5ea21a7642f5c5f
홍지학 <5226947@hanmail.net>
2024-05-29 09:17:07 +0900

    알고리즘 13414번 문제 풀이(구현, 자료 구조, 해시를 사용한 집합과 맵) 2024. 5. 29

---
+++ algorithmSolve/백준/13414.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+// 풀이.
+// 문제에서는 먼저 입력을 모두 받고 중복을 지운후 맨앞부터 K개를 출력하라고 했으나
+// 이러면 최대 L개를 두번 순회해야했다. 그래서 머리를 굴리기로 했는데 처음 L개를 입력받을때
+// 현재 학번이 몇번 등장했는지를 pair<>를 이용하여 따로 저장했다. 그 이후 한번더 L번을 순회하며
+// 현재 학번이 마지막으로 등장했을때 출력을 하게끔 구현했다. 놀랍게도 이 문제는 26퍼센트짜리였다.
+// 또한 속도가 증가하나 궁금해서 pair<>를 string이 아닌 int로 바꾸었는데 아예 답이 안나왔다.
+// 왜인지 모르겠다.
+
+// 자세한 설명은 주석으로 남긴다.
+
+pair<string, int> board[500000];
+unordered_map<string, int> check;
+int K, L;
+
+void MakeAnswer()
+{
+    // unordered_map과 연계하여 현재 학번의 등장횟수까지 저장한다.
+    for (int i = 0; i < L; ++i)
+    {
+        cin >> board[i].first;
+        board[i].second = check[board[i].first] + 1;
+        ++check[board[i].first];
+    }
+    // 한번더 최대 L번 순회하며 현재 학번이 마지막으로 등장한 때라면 그 학번을 출력한다.
+    for (int i = 0; i < L; ++i)
+    {
+        if (board[i].second == check[board[i].first])
+        {
+            cout << board[i].first << '\n';
+            --K;
+        }
+        if (K == 0)
+            break;
+    }
+}
+
+void Input()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    cin >> K >> L;
+}
+
+int main()
+{
+    Input();
+    MakeAnswer();
+    return 0;
+}
