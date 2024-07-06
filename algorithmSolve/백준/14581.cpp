a8f954ff962819394a2c95640100f19056ebbaed
홍지학 <5226947@hanmail.net>
2024-07-06 14:37:40 +0900

    알고리즘 14581번 문제 풀이(구현) 2024. 7. 6

---
+++ algorithmSolve/백준/14581.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+    cout << ":fan::fan::fan:" << '\n';
+    cout << ":fan::" << str << "::fan:" << '\n';
+    cout << ":fan::fan::fan:" << '\n';
+    return 0;
+}
