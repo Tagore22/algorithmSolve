3a2cf1020a3de64920829c340ba5c2aee136d005
홍지학 <5226947@hanmail.net>
2024-07-17 15:10:05 +0900

    알고리즘 23234번 문제 풀이(구현) 2024. 7. 17

---
+++ algorithmSolve/백준/23234.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+// 비가 와서 그런가 채점이 기다리는중에서 거의 몇분째 넘어가질 않아 이 문제의 경우인지
+// 사이트 서버의 문제인지 하나 더 풀어봤다. 서버의 문제였다.
+
+int main()
+{
+    cout << "The world says hello!" << '\n';
+    return 0;
+}
