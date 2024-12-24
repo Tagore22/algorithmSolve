979fb030df2a7ef55db34df7add8bf587012f9fe
홍지학 <5226947@hanmail.net>
2024-12-24 11:46:20 +0900

    알고리즘 4562번 문제 풀이(구현) 2024. 12. 24

---
+++ algorithmSolve/백준/4562.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int N, a, b;
+    cin >> N;
+    for (int i = 0; i < N; ++i)
+    {
+        cin >> a >> b;
+        cout << (a >= b ? "MMM BRAINS" : "NO BRAINS") << '\n';
+    }
+    return 0;
+}
