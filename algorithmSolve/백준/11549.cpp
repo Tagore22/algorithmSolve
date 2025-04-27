c2e533e3537ba771c01028717cd12c8d43aa7258
홍지학 <5226947@hanmail.net>
2025-04-27 11:24:31 +0900

    알고리즘 11549번 문제 풀이(구현) 2025. 4. 27

---
+++ algorithmSolve/백준/11549.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int T, answer = 0;
+    cin >> T;
+    for (int i = 0; i < 5; ++i)
+    {
+        int num;
+        cin >> num;
+        if (num == T)
+            ++answer;
+    }
+    cout << answer << '\n';
+    return 0;
+}
