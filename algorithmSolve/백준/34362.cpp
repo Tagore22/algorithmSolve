45b463c6c1c19047069619bed144e3636cc74781
홍지학 <5226947@hanmail.net>
2025-10-19 15:04:04 +0900

    알고리즘 34362번 문제 풀이(수학, 사칙연산) 2025. 10. 19

---
+++ algorithmSolve/백준/34362.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double N;
+    cin >> N;
+    cout << fixed;
+    cout.precision(4);
+    cout << N - 0.3 << '\n';
+    return 0;
+}
